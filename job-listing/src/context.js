170b9d3ea282422dba313846d7f51bbd2eacb65e
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2021-01-20 18:48:13 +0100

    Rendering

--- job-listing/src/context.js
+++ job-listing/src/context.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {useState, useEffect, useContext} from 'react';
-import data from './data'
+import {data} from './data'
 
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
 
+    const [people, setPeople] = useState(data);
+    const [filterOpen, setFilterOpen] = useState(false);
+
     return (
         <AppContext.Provider value={{
-
+            people
         }}>
             {children}
         </AppContext.Provider>
